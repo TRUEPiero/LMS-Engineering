8743db6f2773586d453f14866259fa61a0d72400
Vladislav Akbashev <akdashevvladunin@gmail.com>
2024-04-06 15:52:49 +0500

    ~

--- Engineering/Study/models.py
+++ Engineering/Study/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Education_materials(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    title = models.CharField(max_length=255)
+    discription = models.TextField(blank=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    file = models.FileField(blank=True)
+    is_first = models.BooleanField(default=False)
+    is_last = models.BooleanField(default=False)
