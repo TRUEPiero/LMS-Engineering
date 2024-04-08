aeec96273937aeb806a04bfdae1ccaed755c79c0
Vladislav Akbashev <akdashevvladunin@gmail.com>
2024-04-08 13:55:26 +0500

    complete project

--- Engineering/Study/urls.py
+++ Engineering/Study/urls.py
@@ -6,9 +6,7 @@ urlpatterns = [
     path('exercise/<slug:ex_slug>/', views.show_exercise, name='exercise'),
     path('new_exercise/<slug:module_slug>', views.new_exercise, name='new_exercise'),
 
-<<<<<<< HEAD
+
     path('new_exercise_post/', views.new_exercise_post, name='new_exercise_post')
-=======
-    path('/', views.new_exercise_post, name='new_exercise_post')
->>>>>>> 8894846d1b4d7ce2a22ffa613850fa44dd76b4ca
+
 ]
